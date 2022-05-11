bd07017afd3a0f66b2ba418ecde49082aae548ae
Douglas <Douglas@Douglas>
2022-05-11 10:35:06 -0300

    mave

--- enviando-email/src/main/java/enviando/email/App.java
+++ enviando-email/src/main/java/enviando/email/App.java
@@ -1,13 +1,15 @@
 package enviando.email;
 
+import junit.framework.Test;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	@Test
+    public void testeEmail() {
+    	
     }
 }
