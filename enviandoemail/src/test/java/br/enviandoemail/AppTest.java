7bd0965dfeeded2f581f5bfca754faf24884a1c5
Douglas <Douglas@Douglas>
2022-05-11 10:55:49 -0300

    teste

---
+++ enviandoemail/src/test/java/br/enviandoemail/AppTest.java
@@ -0,0 +1,17 @@
+package br.enviandoemail;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@org.junit.Test
+	public void testeEmail() {
+		
+	}
+
+}
