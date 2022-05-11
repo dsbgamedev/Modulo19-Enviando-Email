4950964d484a4d7bd7d2b1b75f64007284432bc2
Douglas <Douglas@Douglas>
2022-05-11 13:39:11 -0300

    MODULO 19 AULA 5

--- enviandoemail/src/test/java/br/enviandoemail/AppTest.java
+++ enviandoemail/src/test/java/br/enviandoemail/AppTest.java
@@ -1,5 +1,11 @@
 package br.enviandoemail;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -9,9 +15,36 @@ import junit.framework.TestSuite;
  */
 public class AppTest {
 	
+	private String userName = "dsbocatto@gmail.com";
+	private String senha = "Fantasma@01";
+	
 	@org.junit.Test
 	public void testeEmail() {
 		
+		try {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");/*Autorização*/
+		properties.put("mail.smtp.starttls", "true");/*Autenticação*/
+		properties.put("mail.smtp.host","smtp.gmail.com");/*Servidor gmail Google*/
+		properties.put("mail.smtp.port", "465");/*Porta do servidor*/
+		properties.put("mail.smtp.socketFactory.port", "465");/*Especifica a porta a ser conectada pelo socket*/
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");/*Classe socket de conexao ao smtp*/
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+		
+		System.out.println(senha);
+		
+		//System.out.println(senha);
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
