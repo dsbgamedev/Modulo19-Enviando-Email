e27c704a57ce3f169fa2d99e3a545477eab4aa39
Douglas <Douglas@Douglas>
2022-05-12 11:15:11 -0300

    MODULO 16 AULA 19.10 CONCLUIDA

--- enviandoemail/src/main/java/br/enviandoemail/ObjetoEnviaEmail.java
+++ enviandoemail/src/main/java/br/enviandoemail/ObjetoEnviaEmail.java
@@ -1,15 +1,35 @@
 package br.enviandoemail;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
 import javax.mail.Address;
 import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class ObjetoEnviaEmail {
 
@@ -30,7 +50,7 @@ public class ObjetoEnviaEmail {
 	
 
 	public void enviarEmail(boolean envioHtml) throws Exception{
-		try {
+		
 			Properties properties = new Properties();
 
 			properties.put("mail.smtp.ssl.trust", "*");
@@ -67,9 +87,84 @@ public class ObjetoEnviaEmail {
 			
 			
 			Transport.send(message);
+		
+	}
+	
+	
+	public void enviarEmailAnexo(boolean envioHtml) throws Exception{
+		
+		Properties properties = new Properties();
 
-		} catch (Exception e) {
-			e.printStackTrace();
+		properties.put("mail.smtp.ssl.trust", "*");
+		properties.put("mail.smtp.auth", "true");/* Autorização */
+		properties.put("mail.smtp.starttls", "true");/* Autenticação */
+		properties.put("mail.smtp.host", "smtp.gmail.com");/* Servidor gmail Google */
+		properties.put("mail.smtp.port", "465");/* Porta do servidor */
+		properties.put("mail.smtp.socketFactory.port", "465");/* Especifica a porta a ser conectada pelo socket */
+		properties.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");/* Classe socket de conexao ao smtp */
+
+		// @SuppressWarnings("unused")
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+
+		Address[] toUser = InternetAddress.parse(listaDestinario);
+
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(userName, nomeRemetente));/* Quem está enviando */
+		message.setRecipients(Message.RecipientType.TO, toUser);/* E-mail de destino */
+		message.setSubject(assuntoEmail);/* Assunto do e-mail */
+		
+		/*Parte 1 do email que é texto e a descrição do e-mail*/
+		MimeBodyPart corpoEmail = new MimeBodyPart();
+		
+		if(envioHtml) {
+			corpoEmail.setContent(textoEmail, "text/html; charset=utf-8");
+		}else {
+			corpoEmail.setText(textoEmail);
+
 		}
+		
+		/*Parte 2 do email que são os anexos do PDF*/
+		MimeBodyPart anexoEmail = new MimeBodyPart();
+		
+		//Onde é passado o simulador de PDF voce passa o arquivo gravado no banco de dados
+		anexoEmail.setDataHandler(new DataHandler(new ByteArrayDataSource(simuladorDePDF(), 
+				"application/pdf")));
+		anexoEmail.setFileName("anexoemail.pdf");
+		
+		Multipart multipart = new MimeMultipart();//Esta juntando as duas partes 1 e 2 
+		multipart.addBodyPart(corpoEmail);
+		multipart.addBodyPart(anexoEmail);
+		
+		message.setContent(multipart);
+		
+		
+		Transport.send(message);
+}
+	
+	/**
+	 * 
+	 *
+	 * Esse metodo simulador o PDF ou qualquer arquivo que possa ser enviado por anexo no email.
+	 * Voce pode pegar o arquivo no seu banco de dados base64, byte[], Stream de Arquivos.
+	 * Pode estar em um banco de dados, ou pasta.
+	 * Retornar um PDF em Branco com o texto do Pragrafo de exemplo
+	 */
+	private FileInputStream simuladorDePDF() throws Exception{
+		Document document = new Document();
+		File file = new File("fileanexo.pdf");
+		file.createNewFile();
+		PdfWriter.getInstance(document, new FileOutputStream(file));
+		document.open();
+		document.add(new Paragraph("Conteudo do PDF anexo com Java Mail, esse texto e do PDF"));
+		document.close();
+		
+		return new FileInputStream(file);
 	}
 }
--- enviandoemail/src/test/java/br/enviandoemail/AppTest.java
+++ enviandoemail/src/test/java/br/enviandoemail/AppTest.java
@@ -1,12 +1,6 @@
 package br.enviandoemail;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
 
 /**
  * Unit test for simple App.
@@ -20,32 +14,15 @@ public class AppTest {
 		StringBuilder stringBuilderTextoEmail = new StringBuilder();
 		
 		stringBuilderTextoEmail.append("ola apenas teste <br/><br/>");
-		stringBuilderTextoEmail.append("Estamos fazendo varios testes<br/><br/>");
+		stringBuilderTextoEmail.append("Fazendo varios testes<br/><br/>");
 		
-		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("douglas.bocatto@gmail.com", 
+		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail("douglas.bocatto@gmail.com, moraes@cbadiesel.com.br", 
 															"Douglas JDev",
 														    "Testando email",
 														    stringBuilderTextoEmail.toString());
 		
-		enviaEmail.enviarEmail(true);
+		enviaEmail.enviarEmailAnexo(true);
 	}
-	/**
-	 * 
-	 *
-	 * Esse metodo simulador o PDF ou qualquer arquivo que possa ser enviado por anexo no email.
-	 * Voce pode pegar o arquivo no seu banco de dados base64, byte[], Stream de Arquivos.
-	 * Pode estar em um banco de dados, ou pasta.
-	 * Retornar um PDF em Branco com o texto do Pragrafo de exemplo
-	 */
-	private FileInputStream simuladorDePDF() throws Exception{
-		Document document = new Document();
-		File file = new File("fileanexo.pdf");
-		file.createNewFile();
-		PdfWriter.getInstance(document, new FileOutputStream(file));
-		document.open();
-		document.add(new Paragraph("Conteudo do PDF anexo com Java Mail, esse texto e do PDF"));
-		
-		return new FileInputStream(file);
-	}
+	
 
 }
